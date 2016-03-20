d2629ffe285f69e4e3304530b85d05cbbbf741dd
zxyankh <zxyankh@gmail.com>
2016-03-20 12:56:39 +0800

    Change-Id: Ifdf866607457136d59f83ddcfb6666aa8c482960 Signed-off-by: zxyankh <zxyankh@gmail.com>

--- src/defaultPackage/Algorithm.java
+++ src/defaultPackage/Algorithm.java
@@ -4,36 +4,254 @@ import java.awt.event.KeyEvent;
 
 public class Algorithm {
 	private static Object mData[][] = new Object[4][4];
-	public static void handleEvents(int keycode,Object data[][]){
+	private static int mScore = 0;
+
+	public static void handleEvents(int keycode, Object data[][]) {
 		mData = data;
-		if(keycode == KeyEvent.VK_RIGHT){
+		if (keycode == KeyEvent.VK_RIGHT) {
 			keyRight();
+			zeroToNull();
+			randomAddRight();
+		} else if (keycode == KeyEvent.VK_LEFT) {
+			keyLeft();
+			zeroToNull();
+			randomAddLeft();
+		} else if (keycode == KeyEvent.VK_UP) {
+			keyUp();
+			zeroToNull();
+			randomAddUp();
+		} else if (keycode == KeyEvent.VK_DOWN) {
+			keyDown();
+			zeroToNull();
+			randomAddDown();
 		}
-		else if(keycode == KeyEvent.VK_LEFT){
-			
-		}
-		else if(keycode == KeyEvent.VK_UP){
-			
-		}
-		else if(keycode == KeyEvent.VK_DOWN){
-			
-		}
+
 	}
-	
-	public boolean isEnd(){
-		for(int i = 0; i < 4; i++){
-			for(int j = 0; j < 4 ; j++)
-				if(mData[i][j] == null)
+
+	public static boolean isFull() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (mData[i][j] == null)
 					return false;
+			}
 		}
 		return true;
 	}
-	
-	private static void keyRight(){
-		for(int i = 0; i < 4; i++){
-			for(int j = 0; j < 4; j++){
-				
+
+	public static boolean isEnd() {
+		int num[][] = new int[4][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (mData[i][j] == null)
+					return false;
+				else {
+					num[i][j] = (int) mData[i][j];
+				}
+			}
+		}
+		int i = 0, j = 0;
+		if (num[0][0] == num[0][1] || num[0][0] == num[1][0])
+			return false;
+		if (num[0][3] == num[0][2] || num[0][3] == num[1][3])
+			return false;
+		if (num[3][0] == num[3][1] || num[3][0] == num[2][0])
+			return false;
+		if (num[3][3] == num[3][2] || num[3][3] == num[2][3])
+			return false;
+		for (i = 1; i < 3; i++) {
+			if (num[i][0] == num[i - 1][0] || num[i][0] == num[i][1] || num[i][0] == num[i + 1][0])
+				return false;
+			if (num[i][3] == num[i - 1][3] || num[i][3] == num[i][2] || num[i][3] == num[i + 1][3])
+				return false;
+		}
+		for (j = 1; j < 3; i++) {
+			if (num[0][j] == num[0][j - 1] || num[0][j] == num[1][j] || num[0][j] == num[0][j + 1])
+				return false;
+			if (num[3][j] == num[3][j - 1] || num[3][j] == num[2][j] || num[3][j] == num[3][j + 1])
+				return false;
+		}
+		for (i = 1; i < 3; i++) {
+			for (int k = 1; k < 3; k++) {
+				if (num[i][k] == num[i - 1][k] || num[i][k] == num[i][k - 1] || num[i][k] == num[i][k + 1]
+						|| num[i][k] == num[i + 1][k])
+					return false;
+			}
+		}
+		return true;
+	}
+
+	private static void zeroToNull() {
+		for (int i = 0; i < 4; i++) {
+			int num[] = new int[4];
+			for (int j = 0; j < 4; j++) {
+				if ((int) mData[i][j] == 0)
+					mData[i][j] = null;
 			}
 		}
 	}
+
+	private static void keyRight() {
+		for (int i = 0; i < 4; i++) {
+			int num[] = new int[4];
+			for (int j = 0; j < 4; j++) {
+				if (mData[i][j] == null)
+					mData[i][j] = 0;
+				num[j] = (int) mData[i][j];
+			}
+			for (int j = 3; j >= 0; j--) {
+				if (j != 0 && num[j] != 0) {
+					if (num[j - 1] == num[j]) {
+						num[j] += num[j - 1];
+						num[j - 1] = 0;
+						mScore += num[j];
+					}
+				}
+				if (j != 3 && num[j + 1] == 0) {
+					num[j + 1] = num[j];
+					num[j] = 0;
+				}
+			}
+			for (int j = 0; j < 4; j++) {
+				mData[i][j] = num[j];
+			}
+		}
+	}
+
+	private static void keyLeft() {
+		for (int i = 0; i < 4; i++) {
+			int num[] = new int[4];
+			for (int j = 0; j < 4; j++) {
+				if (mData[i][j] == null)
+					mData[i][j] = 0;
+				num[j] = (int) mData[i][j];
+			}
+			for (int j = 0; j <= 3; j++) {
+				if (j != 3 && num[j] != 0) {
+					if (num[j + 1] == num[j]) {
+						num[j] += num[j + 1];
+						num[j + 1] = 0;
+						mScore += num[j];
+					}
+				}
+				if (j != 0 && num[j - 1] == 0) {
+					num[j - 1] = num[j];
+					num[j] = 0;
+				}
+			}
+			for (int j = 0; j < 4; j++) {
+				mData[i][j] = num[j];
+			}
+		}
+	}
+
+	private static void keyDown() {
+		for (int i = 0; i < 4; i++) {
+			int num[] = new int[4];
+			for (int j = 0; j < 4; j++) {
+				if (mData[j][i] == null)
+					mData[j][i] = 0;
+				num[j] = (int) mData[j][i];
+			}
+			for (int j = 3; j >= 0; j--) {
+				if (j != 0 && num[j] != 0) {
+					if (num[j - 1] == num[j]) {
+						num[j] += num[j - 1];
+						num[j - 1] = 0;
+						mScore += num[j];
+					}
+				}
+				if (j != 3 && num[j + 1] == 0) {
+					num[j + 1] = num[j];
+					num[j] = 0;
+				}
+			}
+			for (int j = 0; j < 4; j++) {
+				mData[j][i] = num[j];
+			}
+		}
+	}
+
+	private static void keyUp() {
+		for (int i = 0; i < 4; i++) {
+			int num[] = new int[4];
+			for (int j = 0; j < 4; j++) {
+				if (mData[j][i] == null)
+					mData[j][i] = 0;
+				num[j] = (int) mData[j][i];
+			}
+			for (int j = 0; j <= 3; j++) {
+				if (j != 3 && num[j] != 0) {
+					if (num[j + 1] == num[j]) {
+						num[j] += num[j + 1];
+						num[j + 1] = 0;
+						mScore += num[j];
+					}
+				}
+				if (j != 0 && num[j - 1] == 0) {
+					num[j - 1] = num[j];
+					num[j] = 0;
+				}
+			}
+			for (int j = 0; j < 4; j++) {
+				mData[j][i] = num[j];
+			}
+		}
+	}
+
+	private static void randomAddDown() {
+		// TODO Auto-generated method stub
+		while (true) {
+			int random = (int) (Math.random() * 4);
+			if (mData[0][random] == null) {
+				mData[0][random] = 2;
+				break;
+			}
+		}
+	}
+
+	private static void randomAddUp() {
+		// TODO Auto-generated method stub
+		while (true) {
+			int random = (int) (Math.random() * 4);
+			if (mData[3][random] == null) {
+				mData[3][random] = 2;
+				break;
+			}
+		}
+	}
+
+	private static void randomAddLeft() {
+		// TODO Auto-generated method stub
+		while (true) {
+			int random = (int) (Math.random() * 4);
+			if (mData[random][3] == null) {
+				mData[random][3] = 2;
+				break;
+			}
+		}
+	}
+
+	private static void randomAddRight() {
+		// TODO Auto-generated method stub
+		while (true) {
+			int random = (int) (Math.random() * 4);
+			if (mData[random][0] == null) {
+				mData[random][0] = 2;
+				break;
+			}
+		}
+	}
+
+	public static int getScore() {
+		return mScore;
+	}
+
+	public static void resetScore() {
+		mScore = 0;
+	}
+
+	public static Object[][] getData() {
+		return mData;
+	}
+
 }
--- src/defaultPackage/MainUI.java
+++ src/defaultPackage/MainUI.java
@@ -4,32 +4,50 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
-public class MainUI extends JFrame implements KeyListener{
-	JPanel panel = new JPanel();
-	JLabel lblCurrent = new JLabel("\u5F53\u524D\u5206\u6570\uFF1A");
-	JLabel lblTop = new JLabel("\u6700\u9AD8\u5206\u6570\uFF1A");
-	JButton btnRestart = new JButton("\u91CD\u65B0\u5F00\u59CB");
+public class MainUI extends JFrame implements KeyListener {
+	private JPanel panel = new JPanel();
+	private JLabel lblCurrent = new JLabel("当前分数：");
+	private JLabel lblTop = new JLabel("最高分数：");
+	private JButton btnRestart = new JButton("重新开始");
 	private static JTable tbl;
 	private static Object tblData[][] = new Object[4][4];
 	static DefaultTableModel tblModel = new DefaultTableModel();
 
-	public static void reloadTbl() {
+	public void reloadTbl() {
 		tblModel.setDataVector(tblData, tblData);
 		tbl.validate();
 		tbl.repaint();
-
 	}
 
+	private void resetTbl(){
+		int count = 0;
+		tblData = new Object[4][4];
+		while(count != 5){
+			int row = (int)(Math.random() * 4);
+			int column = (int)(Math.random() * 4);
+			if(tblData[row][column] == null){
+				tblData[row][column] = 2;
+	    		count++;
+			}
+		}
+		reloadTbl();
+	}
+	
 	public MainUI() {
 		getContentPane().add(panel, BorderLayout.SOUTH);
 		panel.add(lblCurrent);
@@ -40,6 +58,17 @@ public class MainUI extends JFrame implements KeyListener{
 		tbl.addKeyListener(this);
 		getContentPane().add(tbl, BorderLayout.CENTER);
 		reloadTbl();
+		btnRestart.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				Algorithm.resetScore();
+				int score = Algorithm.getScore();
+				lblCurrent.setText("当前分数：" + score);
+				resetTbl();
+			}
+		});
 		this.setTitle("2048");
 		this.setSize(300, 250);
 		// 屏幕居中显示
@@ -57,9 +86,12 @@ public class MainUI extends JFrame implements KeyListener{
 	}
 
 	public static void main(String[] args) {
+		/* 测试数据 */
 		tblData[0][1] = 2;
 		tblData[0][2] = 2;
-		tblData[0][3] = 4;
+		tblData[0][3] = 2;
+		tblData[1][1] = 2;
+		tblData[2][1] = 2;
 		new MainUI();
 	}
 
@@ -67,17 +99,26 @@ public class MainUI extends JFrame implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		Algorithm.handleEvents(e.getKeyCode(), tblData);
+		tblData = Algorithm.getData();
+		int score = Algorithm.getScore();
+		this.lblCurrent.setText("当前分数：" + score);
+		this.reloadTbl();
+		if(Algorithm.isFull())
+			System.out.println("full");
+		if(Algorithm.isEnd()){
+			JOptionPane.showMessageDialog(this, "游戏结束！", "通知", EXIT_ON_CLOSE, null);
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
